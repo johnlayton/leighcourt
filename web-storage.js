453c83c8f2bdf5657aaf49fc64ddcb02418c1d04
john layton <johnstewartlayton@gmail.com>
2015-03-01 20:34:58 +1100

    update to start using pouchdb

--- web-storage.js
+++ web-storage.js
@@ -20,8 +20,40 @@ Polymer('web-storage', {
     storage: undefined,
 
     ready: function() {
-        var self = this;
+
+      function Cache( name ) {
+        var db = new PouchDB( name );
+        this.add = function( key, value ) {
+          db.put(value, key, function(err, res) {
+            if( err ) {
+              console.log( err );
+            } else {
+              console.log( res );
+            }
+          } );
+        };
+        this.del = function( key ) {
+
+        };
+        this.get = function( key, success, failure ) {
+          db.get( key, function( err, doc ) {
+            if ( err ) {
+              failure();
+            } else {
+              success( doc );
+            }
+          } );
+        };
+      }
+
+      this.storage = new Cache( this.group.replace( / /, '_' ) + '_' +
+                                this.resource.replace( / /, '_' ) );
+
+      this.fire('storage-ready', { storage : this.storage });
+
+      /*
         var evt_manager = leighcourt_events();
+
         leighcourt_database( {
           window   : window,
           group    : self.group,
@@ -29,14 +61,21 @@ Polymer('web-storage', {
           events   : evt_manager
         } );
 
+        console.log( "Create Storage ... " );
+        console.log( self.resource );
+
         (function (emr) {
             emr.on('storageLoaded', 'storageReady');
             emr.fire('storageLoad');
             emr.on('storageReady', function (storage) {
+
+                console.log( "Storage Loaded ..." );
+                console.log( storage );
+
                 self.storage = storage;
                 self.fire('storage-ready', {'storage':storage});
             });
         })(evt_manager);
-
+      */
     }
 });
